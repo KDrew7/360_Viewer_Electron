75d67f4672b5abb54a7412314f49181501bee684
KDrew7 <kdrew7@live.com>
2022-06-04 16:07:42 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('sup');
+const { app, BrowserWindow } = require('electron');
+//npm install --save-dev electron
+// const {app, BrowserWindow} = require('electron');
+const electron = require('electron');
+
+app.on('ready',function(){
+
+    var win = new BrowserWindow({width: 1280, height: 720});
+
+    win.loadURL('index.html');
+});
